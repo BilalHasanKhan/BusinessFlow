3f693ce0c92080ef67957862af70845a06ffd395
Bilal Khan <bilalhasankhan@gmail.com>
2012-05-01 23:48:51 +0100

    commented handleerror filter

--- BusinessFlow/Global.asax.cs
+++ BusinessFlow/Global.asax.cs
@@ -18,7 +18,7 @@ namespace BusinessFlow
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
            // filters.Add(new LogonAuthorize());
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
         }
 
         public static void RegisterRoutes(RouteCollection routes)
